9c92d473c5f6618f913bdf80e89dca8f9f2f0a09
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-17 15:50:11 +0100

    day01 solved without test

---
+++ week13-labs-task/src/main/java/day01/ClassNoteBook.java
@@ -0,0 +1,34 @@
+package day01;
+
+import java.util.*;
+
+public class ClassNoteBook {
+
+    private Map<Student, List<Integer>> marks = new TreeMap<>(Student::compareTo);
+
+    public void addStudent(Student student) {
+        marks.put(student, new ArrayList<>());
+    }
+
+    public void addMark(int id, int mark) {
+        validateParameters(id, mark);
+        for (Map.Entry<Student, List<Integer>> entry : marks.entrySet()) {
+            if (id == entry.getKey().getId()) {
+                entry.getValue().add(mark);
+            }
+        }
+    }
+
+    private void validateParameters(int id, int mark) {
+        if (mark < 1 || mark > 5) {
+            throw new IllegalArgumentException("Invalid mark!");
+        }
+        Set<Integer> listOfStudentId = new TreeSet<>();
+        for (Map.Entry<Student, List<Integer>> entry : marks.entrySet()) {
+            listOfStudentId.add(entry.getKey().getId());
+        }
+        if (!listOfStudentId.contains(id)) {
+            throw new IllegalArgumentException("No such student!");
+        }
+    }
+}
