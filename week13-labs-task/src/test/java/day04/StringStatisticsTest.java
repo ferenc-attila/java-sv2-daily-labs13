b0a5215393692b40e986880c9d230fafa1615084
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-20 16:19:58 +0100

    day04 test added

--- week13-labs-task/src/test/java/day04/StringStatisticsTest.java
+++ week13-labs-task/src/test/java/day04/StringStatisticsTest.java
@@ -2,6 +2,9 @@ package day04;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringStatisticsTest {
@@ -9,5 +12,10 @@ class StringStatisticsTest {
     @Test
     void getNumberOfVowelsTest() {
         StringStatistics statistics = new StringStatistics();
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('a',1);
+        expected.put('e',3);
+        expected.put('i',3);
+        assertEquals(expected, statistics.getNumberOfVowels("This is a text in screen!"));
     }
 }
